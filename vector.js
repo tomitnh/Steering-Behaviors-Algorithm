6c7bba19ec26fa112989e1bc203307fd401ca546
Tomit Huynh <tomitnh@gmail.com>
2018-04-16 20:35:27 -0400

    Implemented vector calculations

--- vector.js
+++ vector.js
@@ -1,8 +1,5 @@
 /* 2D Physic calculations
- * TODO:
- * - mult()
- * - setMag() 
- * - sub() 
+ * normalize() and mag() calc: https://www.youtube.com/watch?v=uHusbFmq-4I
  */
 class Vector {
 	constructor(x, y) {
@@ -13,5 +10,37 @@ class Vector {
 	add(p) {
 		this.x += p.x;
 		this.y += p.y;
+	}
+
+	sub(p) {
+		this.x -= p.x;
+		this.y -= p.y;
+	}
+
+	copy() {
+		return new Vector(this.x, this.y);
+	}
+
+	// Multiply by a scalar
+	mult(s) {
+		this.x *= s;
+		this.y *= s;
+	}
+
+	setMag(m) {
+		this.normalize();
+		this.mult(m);
+	}
+
+	mag() {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+
+	normalize () {
+		var m = this.mag();
+		if (m != 0) {
+			this.x = this.x/m;
+			this.y = this.y/m;
+		}
 	}
 }
